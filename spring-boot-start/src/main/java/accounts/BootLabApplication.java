0d3b26a627a271ca9d017310e82d14688dfe6819
livhuwanim <livhuwani.madzivhandila@mmiholdings.co.za>
2017-10-11 20:06:56 +0200

    SPRING- BOOT

--- spring-boot-start/src/main/java/accounts/BootLabApplication.java
+++ spring-boot-start/src/main/java/accounts/BootLabApplication.java
@@ -2,12 +2,20 @@ package accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import accounts.internal.JpaAccountManager;
 
 @SpringBootApplication
 public class BootLabApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BootLabApplication.class, args);
+    }
+    
+    @Bean
+    public AccountManager accountManager(){
+    return new JpaAccountManager();
     }
 
 }
